09b186a528a878561a5751403bb869450583e7cf
YunChan <juleefang94@gmail.com>
2022-09-19 22:00:26 +0700

    deloy lên server

---
+++ build/precache-manifest.644650f7c9eb7ecf7b0e19ae901a9015.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83897c6574b9d14697b3a60838bedcff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d037a396b6a951d7a84",
+    "url": "/static/css/3.6365a31b.chunk.css"
+  },
+  {
+    "revision": "6df6a252182f4b8ce242",
+    "url": "/static/css/4.3c82d501.chunk.css"
+  },
+  {
+    "revision": "eacdbe7e6cb6281907f9",
+    "url": "/static/css/main.4afe85e0.chunk.css"
+  },
+  {
+    "revision": "351244ed31b2ddb420dc",
+    "url": "/static/js/0.fd47da27.chunk.js"
+  },
+  {
+    "revision": "a73ae50cc0b9f13c21dd696a093a7ee8",
+    "url": "/static/js/0.fd47da27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d037a396b6a951d7a84",
+    "url": "/static/js/3.e3ecaa39.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/3.e3ecaa39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df6a252182f4b8ce242",
+    "url": "/static/js/4.687f0385.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/4.687f0385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51703d79cda5f347ef3",
+    "url": "/static/js/5.276c05b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/5.276c05b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacdbe7e6cb6281907f9",
+    "url": "/static/js/main.a11c15f4.chunk.js"
+  },
+  {
+    "revision": "a04af70d0d7fc9df3d39",
+    "url": "/static/js/runtime-main.d23912f2.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Browse from "./pages/browse/Browse";
-import Search from "./pages/search/Search";
+// import Browse from "./pages/browse/Browse";
+// import Search from "./pages/search/Search";
+// Browser chi download code khi user su dung trang do
+const Browse = React.lazy(() => import("./pages/browse/Browse"));
+const Search = React.lazy(() => import("./pages/search/Search"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Browse />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<p className="centered">Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<Browse />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
